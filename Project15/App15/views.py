53c1e7870e521f1670bf07da60bb5ecc1ff38985
rathswayambhu <rath.swayambhu@gmail.com>
2021-06-22 02:43:04 +0530

    21 June

---
+++ Project15/App15/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from App15.models import Registration
+
+
+# Create your views here.
+def showMainPage(request):
+    return render(request,"main.html")
+
+
+def showRegisterPage(request):
+    if request.method == "POST":
+        n = request.POST.get("name")
+        con = request.POST.get("contact")
+        loc = request.POST.get("location")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        Registration(name=n,contact=con,location=loc,email=email,password=password).save()
+
+        return render(request,"register.html",{"message":"Registration is Done"})
+
+    else:
+        return render(request,"register.html")
+
+
+def showLoginPage(request):
+    if request.method == "GET":
+        return render(request,"login.html")
+    else:
+        em = request.POST.get("email")
+        pas = request.POST.get("password")
+
+        try:
+            stu_obj = Registration.objects.get(email=em,password=pas)
+            return render(request,"welcome.html")
+        except Registration.DoesNotExist:
+            return render(request,"login.html",{"error_message":"Invalid User"})
